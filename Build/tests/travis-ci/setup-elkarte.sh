9374d58dff0b5e4edfd01746f3d0d701aa41ad8a
Joshua Dickerson <joshua.a.dickerson@gmail.com>
2016-01-05 17:37:09 -0500

    * remove more sudo usage and try to fix some paths

--- Build/tests/travis-ci/setup-elkarte.sh
+++ Build/tests/travis-ci/setup-elkarte.sh
@@ -11,25 +11,26 @@ set -x
 DB=$1
 TRAVIS_PHP_VERSION=$2
 SHORT_PHP=${TRAVIS_PHP_VERSION:0:3}
+ELKDIR=./Elkarte
 
 # Rename ElkArte test files so they can be used by the install
-mv ./Settings.sample.php ./Settings.php
-mv ./Settings_bak.sample.php ./Settings_bak.php
-mv ./db_last_error.sample.txt ./db_last_error.txt
+mv ./Elkarte/Settings.sample.php ./Elkarte/Settings.php
+mv ./Elkarte/Settings_bak.sample.php ./Elkarte/Settings_bak.php
+mv ./Elkarte/db_last_error.sample.txt ./Elkarte/db_last_error.txt
 
 # Move everything to the www directory so Apache can find it
-sudo mv * /var/www/
-cd /var/www
+#mv * /var/www/
+#cd /var/www
 
 # Yes but its a test run
-sudo chmod -R 777 /var/www
+chmod -R 777 /var/www
 
 # Install the right database for this run
-if [ "$DB" == "mysqli" ]; then sudo php ./tests/travis-ci/setup_mysql.php; fi
-if [ "$DB" == "postgres" ]; then sudo php ./tests/travis-ci/setup_pgsql.php; fi
+if [ "$DB" == "mysqli" ]; then php ./Build/tests/travis-ci/setup_mysql.php; fi
+if [ "$DB" == "postgres" ]; then php ./Build/tests/travis-ci/setup_pgsql.php; fi
 
 # Remove the install dir
-sudo rm -rf /var/www/install
+rm -rf /var/www/Elkarte/src/Admin/Install
 
 # Load in phpunit and its dependencies via composer, note we have a lock file in place
 # composer is updated in setup-server.sh
@@ -38,15 +39,15 @@ then
     composer install --no-interaction --prefer-source --quiet
 
     # Update the added phpunit files
-    sudo chmod -R 777 /var/www/vendor
+    #sudo chmod -R 777 /var/www/vendor
 
     # common php.ini updates (if any)
-    phpenv config-add /var/www//tests/travis-ci/travis_php.ini
+    phpenv config-add /var/www/Build/tests/travis-ci/travis_php.ini
 
     # If this is a code coverage run, we need to enable selenium and capture its coverage results
-    if [ "$SHORT_PHP" == "5.4" -a "$DB" == "mysqli" ]
+    if [ "$SHORT_PHP" == "5.5" -a "$DB" == "mysqli" ]
     then
-	    phpenv config-add /var/www//tests/travis-ci/travis_webtest_php.ini
-	    sudo ./tests/travis-ci/setup-selenium.sh
+	    phpenv config-add /var/www/Build/tests/travis-ci/travis_webtest_php.ini
+	    ./Build/tests/travis-ci/setup-selenium.sh
     fi
 fi

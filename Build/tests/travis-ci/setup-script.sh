5d2199b8fc5a0a3a49389085e0de6a586f6eb795
Joshua Dickerson <joshua.a.dickerson@gmail.com>
2016-01-05 21:00:07 -0500

    travis

--- Build/tests/travis-ci/setup-script.sh
+++ Build/tests/travis-ci/setup-script.sh
@@ -10,14 +10,14 @@ TRAVIS_PHP_VERSION=$2
 SHORT_PHP=${TRAVIS_PHP_VERSION:0:3}
 
 # Run phpunit tests for the site
-if [ "$SHORT_PHP" == "5.4" -a "$DB" == "mysqli" ]
+if [ "$SHORT_PHP" == "5.5" -a "$DB" == "mysqli" ]
 then
-    /var/www/vendor/bin/phpunit --configuration /var/www/tests/travis-ci/phpunit-with-coverage-$DB-travis.xml
-    /var/www/vendor/bin/phpunit /var/www/tests/travis-ci/BootstrapRunTest.php
+    phpunit --configuration Build/tests/travis-ci/phpunit-with-coverage-$DB-travis.xml
+    phpunit /var/www/tests/travis-ci/BootstrapRunTest.php
 elif [ "$DB" == "none" ]
 then
-    /var/www/vendor/bin/phpunit --configuration /var/www/tests/travis-ci/phpunit-basic-travis.xml
+    phpunit --configuration Build/tests/travis-ci/phpunit-basic-travis.xml
 else
-    /var/www/vendor/bin/phpunit --configuration /var/www/tests/travis-ci/phpunit-$DB-travis.xml
-    /var/www/vendor/bin/phpunit /var/www/tests/travis-ci/BootstrapRunTest.php
+    phpunit --configuration Build/tests/travis-ci/phpunit-$DB-travis.xml
+    phpunit Build/tests/travis-ci/BootstrapRunTest.php
 fi
